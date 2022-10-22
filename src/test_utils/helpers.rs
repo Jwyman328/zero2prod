ca1d0ccf5fd31570d5e31b31615df2c9398b401a
Joseph Wyman <jwymandev@gmail.com>
2022-10-22 14:25:57 -0400

    run fmt

--- src/test_utils/helpers.rs
+++ src/test_utils/helpers.rs
@@ -8,10 +8,7 @@ pub fn spawn_app() -> std::net::SocketAddr {
     let test_server_address = "127.0.0.1:0";
     let tcp_listener =
         TcpListener::bind(test_server_address).expect("tcp listener spawn app failed");
-    let tcp_port = tcp_listener
-        .local_addr()
-        .expect("unwrap local addr");
-
+    let tcp_port = tcp_listener.local_addr().expect("unwrap local addr");
 
     let server = run(tcp_listener).expect("error starting server");
     let _ = tokio::spawn(server);
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,13 +8,13 @@ use zero2prod::test_utils::helpers::spawn_app;
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
-     // port 0 tells the OS go find me an available port.
+    // port 0 tells the OS go find me an available port.
     let tcp_port = spawn_app(); // spin up server in other thread
-                                                    // We need to bring in `request`
-                                                    // to perform HTTP request against our app
-                                                    //
-                                                    // Use cargo add reqwest --dev
-                                                    //
+                                // We need to bring in `request`
+                                // to perform HTTP request against our app
+                                //
+                                // Use cargo add reqwest --dev
+                                //
     let client = reqwest::Client::new();
 
     //Act
